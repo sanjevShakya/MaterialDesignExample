dfafcfd797dc69c6ddc655079c768dbef76bdbeb
sshakya <sanjevsakya@gmailcom>
2016-05-23 18:01:20 +0545

    1.0 app_bar and menu

--- app/src/main/java/com/materialex/sshakya/materialdesignexample/MainActivity.java
+++ app/src/main/java/com/materialex/sshakya/materialdesignexample/MainActivity.java
@@ -2,17 +2,24 @@ package com.materialex.sshakya.materialdesignexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar = (Toolbar)findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
     }
 
     @Override
-    protected void onStop() {
-        super.onStop();
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return true;
     }
 }
