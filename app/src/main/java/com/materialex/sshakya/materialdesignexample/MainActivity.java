a42246ffc82a0315df377fe9175853b4ef39dfeb
sshakya <sanjevsakya@gmailcom>
2016-05-23 20:00:46 +0545

    1.04 drawerlayout initial

--- app/src/main/java/com/materialex/sshakya/materialdesignexample/MainActivity.java
+++ app/src/main/java/com/materialex/sshakya/materialdesignexample/MainActivity.java
@@ -1,6 +1,7 @@
 package com.materialex.sshakya.materialdesignexample;
 
 import android.content.Intent;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -18,6 +19,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         toolbar = (Toolbar)findViewById(R.id.app_bar);
         setSupportActionBar(toolbar);
+
+        NavigationDrawerFragment drawerFragment = (NavigationDrawerFragment)getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
+        drawerFragment.setUp((DrawerLayout)findViewById(R.id.drawer_layout),toolbar);
+
     }
 
     @Override
