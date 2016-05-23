6087e30db51e358e1e2eb9c6b4100be7d0874dbd
sshakya <sanjevsakya@gmailcom>
2016-05-23 19:21:52 +0545

    1.03 navigation toolbar to next SubActivity

--- app/src/main/java/com/materialex/sshakya/materialdesignexample/SubActivity.java
+++ app/src/main/java/com/materialex/sshakya/materialdesignexample/SubActivity.java
@@ -1,6 +1,7 @@
 package com.materialex.sshakya.materialdesignexample;
 
 import android.content.Intent;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -8,21 +9,23 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class SubActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        toolbar = (Toolbar)findViewById(R.id.app_bar);
+        setContentView(R.layout.activity_sub);
+        toolbar=(Toolbar)findViewById(R.id.app_bar);
         setSupportActionBar(toolbar);
-    }
 
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main,menu);
+        getMenuInflater().inflate(R.menu.menu_sub,menu);
         return true;
     }
 
@@ -33,8 +36,8 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(this, "Setting Hit", Toast.LENGTH_SHORT).show();
             return true;
         }
-        if(id==R.id.navigate){
-            startActivity(new Intent(this,SubActivity.class));
+        if(id== android.R.id.home){
+            NavUtils.navigateUpFromSameTask(this);
         }
         return super.onOptionsItemSelected(item);
     }
