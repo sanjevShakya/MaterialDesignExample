259094a2138f968f2732220e3f033ff35098edad
sshakya <sanjevsakya@gmailcom>
2016-05-23 18:04:17 +0545

    1.01 onOptionItemSeleted

--- app/src/main/java/com/materialex/sshakya/materialdesignexample/MainActivity.java
+++ app/src/main/java/com/materialex/sshakya/materialdesignexample/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,5 +23,15 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main,menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if(id==R.id.action_settings){
+            Toast.makeText(this, "Setting Hit", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
